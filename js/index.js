4bef7b5ba90a23a57b43a24ff5c611919e13401b
Vpsolo <1foreverfarever@gmail.com>
2019-02-20 16:30:43 +0600

    Done menu

--- js/index.js
+++ js/index.js
@@ -12,8 +12,6 @@ $(document).ready(function($) {
     });
 });
 
-
-
 var castle = false;
 if ($(window).width()<990) {
     castle = true;
@@ -23,63 +21,14 @@ else {
 }
 
 if (castle==true) {
-    var a=false;
-    function menu_click_js1(){
-        if (a==true) {
-            $("#menu_1").css("opacity", "0");
-            $("#menu_1").css("visibility", "hidden");
-            $("#menu_1").css("position", "absolute");
-            a=false;    
-        }
-        else if (a==false) {
-            $("#menu_1").css("opacity", "1");
-            $("#menu_1").css("visibility", "visible");
-            $("#menu_1").css("position", "static");
-            $("#menu_1").css("boxShadow", "0px 10px 20px transparent");         
-            a=true;
-        }
-    }  
-}   
-// var h_hght = 170; // высота шапки
-// var h_mrg = 0;    // отступ когда шапка уже не видна
-// var elem = $('#top_nav');
-             
-// $(function(){
- 
-//      var top = $(this).scrollTop();
-     
-//     if(top > h_hght){
-//         elem.css('top', h_mrg);
-
-//     }              
-//     $(window).scroll(function(){
-//         top = $(this).scrollTop();
-//          if ($(window).width()>767) {
-//         if (top+h_mrg < h_hght ) {
-//             elem.css('top', (h_hght-top));
-//             // elem.css('background-color', '#fff');
-//             // elem.css('color', '#fff');
-//            $('#top_nav .navbar-collapse').removeClass('menucollapse')
-            
-//         } else {
-//             elem.css('top', h_mrg);
-//             // elem.css('background-color', '#253B80');
-//             // elem.css('color', 'white');
-//              $('#top_nav .navbar-collapse').addClass('menucollapse')
-//         }}
-//         else elem.css('top', h_mrg);
-        
-//     });
-
-//  window.onresize=function(){
-//        if ($(window).width()<768){
-//         elem.css('top', h_mrg);
-//     } else {
-//         elem.css('top', (h_hght+top));
-//     }
-// }
-// });
-
+    var a=document.querySelectorAll('.menu_click > li');
+    for (var i = 0; i < a.length; i++) {
+        a[i].onclick = click_m;
+    }
+    function click_m() {
+        this.children[1].classList.toggle('q1');
+    }
+}
 
 //$('#openBtn').click(function(){
 //$('#myModal').modal({show:true})
