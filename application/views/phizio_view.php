ea54764c63319f0516086e8f705b11722745c35a
pryanik1 <alymkulov64@gmail.com>
2019-02-23 14:40:22 +0600

    Добавил все в CodeIgniter и сделал все готовым для продолжения работы

---
+++ application/views/phizio_view.php
@@ -0,0 +1,41 @@
+	<div class="content">
+		<div class="container">
+			<div class="row uzi">
+				<div class="title">
+						Физиотерапия
+					</div>
+					<div class="col-md-12">
+						<div class="row">
+							<div class="col-md-5">
+								<div class="main_img" style="background-image:url('<?php echo base_url().'assets/files/medic/врач-узист.jpeg';?>files/8.jpg')">
+							</div>
+							</div>
+							<div class="col-md-7">
+								<div class="text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum veritatis aut nemo modi sequi, eligendi magni! Eum veritatis rerum ipsam alias natus sapiente, expedita ex aperiam maiores perspiciatis pariatur eaque perferendis totam, velit tempore tenetur beatae distinctio vitae est asperiores ad, iusto soluta eveniet. Tenetur sunt blanditiis id itaque nostrum.
+				</div>
+							</div>
+							
+							
+						</div>
+					</div>
+				
+				
+				<div class="med col-md-12">
+						<div class="container">
+					<div class="row">
+							<div class="med_photo col-md-4">
+							<img src="<?php base_url().'assets/files/medic/врач-узист.jpeg'?>" alt="">
+					</div>
+						<div class="med_name col-md-8">
+						<span>Фамильевич Именин</span>
+						<div><i class="fa fa-heart-o"></i> Врач-Узист</div>
+					</div>
+						</div>
+						
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
---
+++ application/views/recipes_view.php
@@ -0,0 +1,125 @@
+<div class="center-recipes">Узнайте стоимость анализов</div>	
+<div class="container">
+		<div class="recipes">
+			<div class="sec1">
+				<select name="" id="">
+							<option value="" selected>Выберите категорию анализа</option>
+							<option value="">item1</option>
+							<option value="">item2</option>
+							<option value="">item3</option>
+							<option value="">item4</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+							<option value="">item5</option>
+						</select>
+						<p>Все анализы (<span>647</span>)</p>		
+						<div class="scroll">
+							<h1>Диагностика анемий</h1>
+							<div>
+								<span>Железо</span>
+								<span>280 сом</span>
+								<input type="checkbox">
+							</div><div>
+								<span>Железо</span>
+								<span>280 сом</span>
+								<input type="checkbox">
+							</div><div>
+								<span>Железо</span>
+								<span>280 сом</span>
+								<input type="checkbox">
+							</div><div>
+								<span>Железо</span>
+								<span>280 сом</span>
+								<input type="checkbox">
+							</div><div>
+								<span>Железо</span>
+								<span>280 сом</span>
+								<input type="checkbox">
+							</div><div>
+								<span>Железо</span>
+								<span>280 сом</span>
+								<input type="checkbox">
+							</div>
+						</div>
+			</div>
+			<div class="sec2">
+				<div class="result-recipe">
+					<h1>Выбрано исследований</h1>
+					<div class="q1-recipe">(1)</div>
+					<div class="result-cotntent">
+						<div>ss</div>
+					</div>
+					<div class="total">
+						<table>
+					<tr>
+						<th class="tab">Итог</th>
+						<th>0 сом</th>
+					</tr>
+					<tr>
+						<th colspan="2"><button>Войти</button></th>
+					</tr>
+				</table>
+					</div>
+				</div>
+			</div>
+			<div class="sec3">
+				
+			</div>
+		</div>
+</div>
