574ccaac22b386458ecd20fcf17bfc3a63d0a923
Vpsolo <1foreverfarever@gmail.com>
2019-02-25 15:47:37 +0600

    Сделал z-index

---
+++ application/controllers/Pages.php
@@ -0,0 +1,77 @@
+<?php
+
+session_start();
+if (isset($_SESSION['language'])!=true) $_SESSION['language'] = 'russian';
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+
+	public function index($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('slider_view');
+		$this->load->view('middle_view');
+		$this->load->view('about_view');
+		$this->load->view('footer_view');
+	}
+	public function results($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('results_view');
+		$this->load->view('footer_view');
+	}
+	public function uzi($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('uzi_view');
+		$this->load->view('footer_view');
+	}
+	public function phizio($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('phizio_view');
+		$this->load->view('footer_view');
+	}
+	public function schedule($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('schedule_view');
+		$this->load->view('footer_view');
+	}
+	public function recipes($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('recipes_view');
+		$this->load->view('footer_view');
+	}
+
+	public function staff($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('staff_view');
+		$this->load->view('footer_view');
+	}
+		public function doctor($id=0)
+	{
+		$this->load->model('Get_model');
+		$this->load->view('head_view');
+		$this->load->view('header_view');
+		$this->load->view('doctor_view');
+		$this->load->view('footer_view');
+	}
+}	
---
+++ application/views/slider_view.php
@@ -0,0 +1,40 @@
+<div class="slider">
+			
+				<div id="demo" class="carousel slide" data-ride="carousel">
+					<ul class="carousel-indicators">
+						<li data-target="#demo" data-slide-to="0" class="active"></li>
+						<li data-target="#demo" data-slide-to="1"></li>
+						<li data-target="#demo" data-slide-to="2"></li>
+					</ul>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="<?php echo base_url().'assets/images/slider/4.jpg'; ?>" alt="">
+							<div class="carousel-caption">
+								<h3>Здоровый ребенек - счастливая семья !</h3>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img src="<?php echo base_url().'assets/images/slider/66.jpg'; ?>" alt="">
+							<div class="carousel-caption">
+								<h3>Мы за здоровый образ жизни !</h3>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img src="<?php echo base_url().'assets/images/slider/77.jpeg'; ?>" alt="">
+							<div class="carousel-caption">
+								<h3>Правильное питание это не диета !</h3>
+							</div>
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#demo" data-slide="prev">
+						<span class="carousel-control-prev-icon"></span>
+					</a>
+					<a class="carousel-control-next" href="#demo" data-slide="next">
+						<span class="carousel-control-next-icon"></span>
+					</a>
+
+
+				</div>
+			
+		</div>
+
